1048b9cf79edd15a5aaa464b0c0ef40c06797264
Cales <cales.junior2002@gmail.com>
2023-04-05 10:05:50 -0300

    feat - add component DataTable

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { Table } from 'antd'
+import React, {useState} from 'react'
+import {Button, Table} from 'antd'
+import CarroCard from "./CarroCard"
+import DefeitoForm from "./Formulario/DefeitoForm";
 
-
+// definindo o componente DataTable
 function DataTable(props) {
+
+    const [selectedCarro, setSelectedCarro] = useState(null)
+
+    const [showDefeitoForm, setShowDefeitoForm] = useState(false);
+
+    const [showVisualizarCard, setShowVisualizarCard] = useState(false);
+
+    function handleEdit(item) {
+        
+    }
+
+    function handleDelete(item) {
+        
+    }
+
+    function handleDefeito(item) {
+        setSelectedCarro(item);
+        setShowDefeitoForm(true);
+    }
+
+    function handleVisualizar(item) {
+        setSelectedCarro(item);
+        setShowVisualizarCard(true);
+    }
+
     //colunas da tabela
     const columns = [
         {
@@ -25,11 +52,40 @@ function DataTable(props) {
             dataIndex: 'preco',
             key: 'preco'
         },
+        {
+            title: 'Ações',
+            key: 'actions',
+            render: (text, item) => (
+                <>
+                    <Button type="primary" onClick={() => handleVisualizar(item)}>Visualizar</Button>
+                    <Button type="default" onClick={() => handleEdit(item)}>Editar</Button>
+                    <Button danger onClick={() => handleDelete(item)}>Excluir</Button>
+                    <Button warning onClick={() => handleDefeito(item)}>Adicionar Defeitos</Button>
+                    {showDefeitoForm && selectedCarro && (
+                        <DefeitoForm carro_id={selectedCarro.id} onClose={() => setShowDefeitoForm(false)} />
+                    )}
+                </>
+            ),
+        },
     ]
 
     return (
-        <Table columns={columns} dataSource={props.data}/>
-    );
+        <div>
+            <Table columns={columns} dataSource={props.data}/>
+
+            {showVisualizarCard && selectedCarro && (
+                <CarroCard
+                    carro={selectedCarro}
+                    onClose={() => {
+                        setSelectedCarro(null)
+                        setShowVisualizarCard(false)
+                    }
+                    }
+                />
+            )}
+        </div>
+
+    )
 }
 export default DataTable
 
